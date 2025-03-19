49b47aa2f75d8425d590034669b4838b22681768
Cristian Devia <c.devia8@gmail.com>
2025-03-18 21:36:00 -0500

    add state for fetch notes

--- lib/notes/presentation/widgets/add_note_dialog.dart
+++ lib/notes/presentation/widgets/add_note_dialog.dart
@@ -44,6 +44,7 @@ class _AddNoteDialogState extends State<AddNoteDialog> {
         );
         final noteCubit = context.read<NoteCubit>();
         noteCubit.createNote(newNote);
+        noteCubit.fetchNoteById(currentUser!.uid);
         Navigator.pop(context);
       }
     } else {
