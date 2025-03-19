fa45862ce2762dfd86412666b1c42ba8d82dcf86
Cristian Devia <c.devia8@gmail.com>
2025-03-18 21:24:04 -0500

    add notes for home

--- lib/notes/presentation/pages/home_page.dart
+++ lib/notes/presentation/pages/home_page.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:note_app/auth/domain/entities/app_user.dart';
 import 'package:note_app/auth/presentation/cubits/auth_cubits.dart';
+import 'package:note_app/notes/presentation/cubits/note_cubits.dart';
+import 'package:note_app/notes/presentation/cubits/note_states.dart';
 import 'package:note_app/notes/presentation/widgets/add_note_dialog.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,6 +15,31 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  AppUser? currentUser;
+  late final noteCubit = context.read<NoteCubit>();
+
+  //on startup
+  @override
+  void initState() {
+    super.initState();
+    getCurrentUser();
+    fetchNotes();
+  }
+
+  void getCurrentUser() async {
+    final authCubit = context.read<AuthCubit>();
+    currentUser = authCubit.currentUser;
+  }
+
+  void fetchNotes() {
+    noteCubit.fetchNoteById(currentUser!.uid);
+  }
+
+  void deleteNote(String noteId) {
+    noteCubit.deleteNote(noteId);
+    fetchNotes();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -26,6 +55,78 @@ class _HomePageState extends State<HomePage> {
           )
         ],
       ),
+      body: BlocBuilder<NoteCubit, NoteState>(builder: (context, state) {
+        //loading notes
+        if (state is NotesLoading && state is NotesUploading) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+
+          // notes loaded
+        } else if (state is NoteLoaded) {
+          final notes = state.notes;
+          print("Las notas: ${notes}");
+          if (notes.isEmpty) {
+            return const Center(
+              child: Text("No hay notas disponibles"),
+            );
+          }
+          return ListView.builder(
+            itemCount: notes.length,
+            itemBuilder: (context, index) {
+              final note = notes[index];
+              return Container(
+                margin: const EdgeInsets.all(10),
+                decoration: BoxDecoration(
+                  color: Theme.of(context).colorScheme.secondary,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Slidable(
+                  key: ValueKey(note.id),
+                  startActionPane:
+                      ActionPane(motion: const DrawerMotion(), children: [
+                    SlidableAction(
+                      borderRadius: const BorderRadius.only(
+                          topLeft: Radius.circular(12),
+                          bottomLeft: Radius.circular(12)),
+                      backgroundColor: Colors.red,
+                      foregroundColor: Colors.white,
+                      icon: Icons.delete,
+                      onPressed: (context) {},
+                    ),
+                    SlidableAction(
+                      backgroundColor: Colors.amber,
+                      foregroundColor: Colors.white,
+                      icon: Icons.edit,
+                      onPressed: (context) {},
+                    )
+                  ]),
+                  child: ListTile(
+                    title: Text(
+                      note.title,
+                      style: const TextStyle(fontWeight: FontWeight.w500),
+                    ),
+                    subtitle: Text(
+                      note.text,
+                    ),
+                    trailing: Text(
+                        "${note.timestamp.day} / ${note.timestamp.month} / ${note.timestamp.year}"),
+                  ),
+                ),
+              );
+            },
+          );
+        }
+
+        // notes error
+        else if (state is NotesError) {
+          return Center(
+            child: Text(state.message),
+          );
+        } else {
+          return const SizedBox();
+        }
+      }),
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).colorScheme.secondary,
         child: Icon(
