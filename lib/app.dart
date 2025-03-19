9497aea2aa7a8dfc80ff05e68a83e816ffcb0764
Cristian Devia <c.devia8@gmail.com>
2025-03-18 20:29:28 -0500

    add multibloc provider

--- lib/app.dart
+++ lib/app.dart
@@ -4,18 +4,28 @@ import 'package:note_app/auth/data/firebase_auth_repository.dart';
 import 'package:note_app/auth/presentation/cubits/auth_cubits.dart';
 import 'package:note_app/auth/presentation/cubits/auth_states.dart';
 import 'package:note_app/auth/presentation/pages/auth_page.dart';
+import 'package:note_app/notes/data/firebase_note_repository.dart';
+import 'package:note_app/notes/presentation/cubits/note_cubits.dart';
 import 'package:note_app/notes/presentation/pages/home_page.dart';
 import 'package:note_app/theme/theme.dart';
 
 class MyApp extends StatelessWidget {
   final authRepo = FirebaseAuthRepository();
+  final noteRepo = FirebaseNoteRepository();
 
   MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return BlocProvider(
-      create: (context) => AuthCubit(authRepo: authRepo)..checkUser(),
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider<AuthCubit>(
+          create: (context) => AuthCubit(authRepo: authRepo)..checkUser(),
+        ),
+        BlocProvider<NoteCubit>(
+          create: (context) => NoteCubit(noteRepository: noteRepo),
+        ),
+      ],
       child: MaterialApp(
         debugShowCheckedModeBanner: false,
         theme: defaultTheme,
